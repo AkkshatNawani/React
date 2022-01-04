f1cd12a1c3590b581a7e3c356fbeb9d9f31136bb
Akkshat Nawani <akkshatnawani@outlook.com>
2022-01-04 19:15:03 +0530

    Day2

--- testapp/src/index.js
+++ testapp/src/index.js
@@ -1,47 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom";
-import './index.css'
+import "./index.css";
+const firstBook = {
+  author: "Héctor García",
+  title : "Ikigai",
+  img:'https://images-na.ssl-images-amazon.com/images/I/41SG4SO4T9L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+}
 
+const secondBook = {
+  author: "Morgan Housel",
+  title: "The Psychology of Money",
+  img: "https://images-eu.ssl-images-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
 function BookList() {
   return (
-    <section class='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={props.img} alt="" />
+      <h2>{props.title}</h2>
+      <h1>{props.author}</h1>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/41SG4SO4T9L._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  />
-);
-
-const Author = () => <h1>Héctor García</h1>;
-
-const Title = () => <h2>Ikigai</h2>;
-
 ReactDom.render(<BookList />, document.getElementById("root"));
